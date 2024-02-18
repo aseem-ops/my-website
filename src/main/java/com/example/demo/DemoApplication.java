fa5bf050f49360a2e074e82f0c6d63c895214617
aseem-ops <aseemanand1@gmail.com>
2024-02-18 15:23:04 -0500

    Added commect in main class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+//main class
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
